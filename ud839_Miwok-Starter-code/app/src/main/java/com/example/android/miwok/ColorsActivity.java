65596878c2165ff72194dc71b82f6ce0d40afcf2
Ravi Ranjan <raviranjan3570@gmail.com>
2019-06-24 15:40:17 +0530

    added up button

--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -29,6 +29,7 @@ public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
         final ArrayList<Word> words = new ArrayList<Word>();
