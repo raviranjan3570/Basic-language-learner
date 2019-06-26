2e22aa689b8fafce1310133b4162c39a64e723f8
Ravi Ranjan <raviranjan3570@gmail.com>
2019-06-27 00:30:40 +0530

    Added all fragments in one screen

--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -17,6 +17,7 @@ package com.example.android.miwok;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -63,5 +64,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(phrasesIntent);
             }
         });
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
     }
 }
--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/SimpleFragmentPagerAdapter.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/SimpleFragmentPagerAdapter.java
@@ -1,6 +1,30 @@
 package com.example.android.miwok;
 
-import android.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 
-public class SimpleFragmentPagerAdapter extends Fragment {
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new NumberFragment();
+        } else if (position == 1) {
+            return new FamilyFragment();
+        } else if (position == 2) {
+            return new ColorsFragment();
+        } else {
+            return new PhrasesFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
 }
